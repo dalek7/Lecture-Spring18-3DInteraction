30d52e760b61a7ea0f3ccfd6306804311517fef9
seung <ncraig@naver.com>
2018-04-17 02:10:34 +0900

    UDP COmm.

---
+++ Unity-Project/HelloUnity3D/Assets/Scripts/UDPReceive.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+// Used for 717300@Hallym Univ.
+// https://forum.unity.com/threads/simple-udp-implementation-send-read-via-mono-c.15900/
+
+public class UDPReceive : MonoBehaviour
+{
+    // receiving Thread
+    Thread receiveThread;
+
+    // udpclient object
+    UdpClient client;
+
+    // public
+    // public string IP = "127.0.0.1"; default local
+    public int port; // define > init
+
+    // infos
+    public string lastReceivedUDPPacket = "";
+    public string allReceivedUDPPackets = ""; // clean up this from time to time!
+
+    private Rigidbody rb;
+    
+
+    // start from shell
+    private static void Main()
+    {
+        UDPReceive receiveObj = new UDPReceive();
+        receiveObj.init();
+
+        string text = "";
+        do
+        {
+            text = Console.ReadLine();
+        }
+        while (!text.Equals("exit"));
+    }
+    // start from unity3d
+    public void Start()
+    {
+
+        init();
+    }
+
+    // OnGUI
+    void OnGUI()
+    {
+        
+        Rect rectObj = new Rect(40, 10, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPReceive\n127.0.0.1 " + port + " #\n"
+                    + "shell> nc -u 127.0.0.1 : " + port + " \n"
+                    + "\nLast Packet: \n" + lastReceivedUDPPacket
+                    + "\n\nAll Messages: \n" + allReceivedUDPPackets
+                , style);
+    }
+
+    // init
+    private void init()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+        
+        // define port
+		port = 12345;
+
+        // status
+        //print("Sending to 127.0.0.1 : " + port);
+        //print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+
+        // ----------------------------
+        // Abhören
+        // ----------------------------
+        // Lokalen Endpunkt definieren (wo Nachrichten empfangen werden).
+        // Einen neuen Thread für den Empfang eingehender Nachrichten erstellen.
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+
+        client = new UdpClient(port);
+        while (true)
+        {
+
+            try
+            {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+                string text = Encoding.UTF8.GetString(data);
+
+                // Den abgerufenen Text anzeigen.
+                print(">> " + text);
+
+                if(text.Trim().Equals("a"))
+                {
+                    Vector3 movement = new Vector3(-100, 0, 0);
+                    rb.AddForce(movement);
+                }
+                else if (text.Trim().Equals("d"))
+                {
+                    Vector3 movement = new Vector3(100, 0, 0);
+                    rb.AddForce(movement);
+                }
+				else if(text.Trim().Equals("w"))
+				{
+					Vector3 movement = new Vector3(0, 0, 100);
+					rb.AddForce(movement);
+				}
+
+
+                // latest UDPpacket
+                lastReceivedUDPPacket = text;
+
+                // ....
+                allReceivedUDPPackets = allReceivedUDPPackets + text;
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    // getLatestUDPPacket
+    // cleans up the rest
+    public string getLatestUDPPacket()
+    {
+        allReceivedUDPPackets = "";
+        return lastReceivedUDPPacket;
+    }
+
+    void OnDisable()
+    {
+        if (receiveThread != null)
+            receiveThread.Abort();
+
+        client.Close();
+    }
+
+
+}
