a76ba8757325f05f975cdbc23b70fe4c7426ceb9
seung <ncraig@naver.com>
2018-05-10 10:53:10 +0900

    Google VR Reticle Pointer

--- Unity-Project/HelloUnity3D/Assets/Scripts/NewBehaviourScript.cs
+++ Unity-Project/HelloUnity3D/Assets/Scripts/NewBehaviourScript.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Collider))]
 public class NewBehaviourScript : MonoBehaviour {
+
+
     public float speed;
     private Rigidbody rb;
     //private Joystick joystick;
 
+	private int count1;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
         speed = 10;
+		count1 = 0;
         //joystick = FindObjectOfType<Joystick>();
-
     }
+
 
     // Update is called once per frame
 
     void Update()
     {
+
         if (Input.GetKeyDown("z"))
             rb.angularDrag = 5.0F;
         else if (Input.GetKeyDown("x"))
@@ -27,25 +33,24 @@ public class NewBehaviourScript : MonoBehaviour {
 
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 movement = new Vector3(0, 1, 0);
-            rb.AddForce(300.0F * movement);
+			Jump(300.0F);
         }
-		#if !UNITY_ANDROID
+		/*
         else if(Input.GetMouseButtonDown(0) )
         {
             print("Jump !");
             Vector3 movement = new Vector3(0, 1, 0);
             rb.AddForce(300.0F * movement);
         }
-		#endif
+		
         else if (Input.GetButton("Fire2"))
         {
             print("Jump with Fire 2 button!");
-            Vector3 movement = new Vector3(0, 1, 0);
-            rb.AddForce(30.0F * movement);
+			Jump(30.0F);
         }
+		*/
 
-
+		/*
 		#if UNITY_ANDROID
 		Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
@@ -61,15 +66,27 @@ public class NewBehaviourScript : MonoBehaviour {
 
         Vector3 movement1 = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement1 * speed);
-
+		*/
     }
 
-    void FixedUpdate()
+	public void Jump()
+	{
+		string buf = string.Format ("Jump>%d", count1++);
+		print(buf);
+		Vector3 movement = new Vector3(0, 1, 0);
+		rb.AddForce(300.0F * movement);
+	}
+
+	public void Jump(float pwr)
     {
-        
-        
-           
+		
+		print(count1++);
+        Vector3 movement = new Vector3(0, 1, 0);
+		rb.AddForce(pwr * movement);
     }
 
-    
+	public void Reset()
+	{	
+		count1 = 0;
+	}
 }
