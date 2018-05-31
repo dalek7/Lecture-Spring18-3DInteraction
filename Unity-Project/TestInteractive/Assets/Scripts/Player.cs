cc0c961fd07034cf55a2ffc9e14f699e1b466d78
seung <ncraig@naver.com>
2018-05-31 11:02:01 +0900

    GVR

---
+++ Unity-Project/TestInteractive/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		speed = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+
+
+        Vector3 movement1 = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement1 * speed);
+
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
+
+	}
+
+	public void Jump()
+	{
+		Vector3 movement = new Vector3(0, 1, 0);
+		rb.AddForce(300.0F * movement);
+		//sound1.Play();
+	}
+}
