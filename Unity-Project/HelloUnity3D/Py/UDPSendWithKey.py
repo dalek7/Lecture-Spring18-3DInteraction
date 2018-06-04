b7e76b703f132e6748a6edcc2e79b5ff122802e1
seung <ncraig@naver.com>
2018-06-04 23:32:53 +0900

    Key input

--- Unity-Project/HelloUnity3D/Py/UDPSendWithKey.py
+++ Unity-Project/HelloUnity3D/Py/UDPSendWithKey.py
@@ -1,29 +1,47 @@
 import socket
+import sys
 from pynput.keyboard import Key, Listener
-#UDP_IP = "192.168.1.200"
-#UDP_PORT = 10003
 
-UDP_IP = "192.168.1.100" #"127.0.0.1"
+''' 
+Script used for 717300, 3D Interaction Design, Hallym University
+
+Example
+
+python UDPSendWithKey.py 192.168.1.100 12345
+
+or simply
+
+python UDPSendWithKey.py 
+'''
+
+UDP_IP = "127.0.0.1"
 UDP_PORT = 12345
 MESSAGE = "Hello, World!"
 
+if len(sys.argv) > 2:
+	UDP_IP = sys.argv[1]
+	UDP_PORT = int(sys.argv[2])
+
+elif len(sys.argv) < 1:	#default
+	UDP_IP = "127.0.0.1"
+	UDP_PORT = 12345
+
 def on_press(key):
-    #print(key)
-    #print('{0}'.format(key))
+
     if key== Key.up:
-        print("Up !")
+        #print("Up !")
         send("w", UDP_IP, UDP_PORT)
     elif key== Key.down:
-        print("Down !")
+        #print("Down !")
         send("s", UDP_IP, UDP_PORT)
     elif key== Key.left:
-        print("Left !")
+        #print("Left !")
         send("a", UDP_IP, UDP_PORT)
     elif key== Key.right:
-        print("Right !")
+        #print("Right !")
         send("d", UDP_IP, UDP_PORT)
     elif key== Key.space:
-        print("Space !")
+        #print("Space !")
         send("j", UDP_IP, UDP_PORT)
 
 
@@ -40,10 +58,11 @@ def send(data, port=12345, addr='127.0.0.1'):
 
     #print("UDP target IP:", UDP_IP)
     #print("UDP target port:", UDP_PORT)
-    print("Sending message: {}".format(data))
+    print("Sending message to {}:{}: {}".format(UDP_IP, UDP_PORT, data))
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-    sock.sendto(bytes(data, "utf-8"), (UDP_IP, UDP_PORT))
+    #sock.sendto(bytes(data, "utf-8"), (UDP_IP, UDP_PORT))
+    sock.sendto(bytes(data.encode("utf-8")), (UDP_IP, UDP_PORT))
 
 # Collect events until released
 with Listener(
